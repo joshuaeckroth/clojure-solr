5a273c75e9dc03202cd16ccd1d1a707862b319f7
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2015-06-12 11:54:02 -0400

    Updated solr dep; bumped version to 0.8.24.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject cc.artifice/clojure-solr "0.8.23"
+(defproject cc.artifice/clojure-solr "0.8.24"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.apache.solr/solr-solrj "4.7.2"]
-                 [org.apache.solr/solr-core "4.7.2"]
-                 [org.slf4j/slf4j-jcl "1.7.7"]
+                 [org.apache.solr/solr-solrj "4.10.4"]
+                 [org.apache.solr/solr-core "4.10.4" :exclusions [commons-fileupload]]
+                 [commons-fileupload "1.3"]
+                 [org.slf4j/slf4j-jcl "1.7.6"]
                  [clj-time "0.9.0"]]
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]}}
   :repositories [["restlet" {:url "http://maven.restlet.org"}]])
